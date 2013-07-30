52cfbc700962f9d52a1543afaec2612721d75e94
henllyee <henllyee@126.com>
2013-07-30 22:42:27 +0800

    create micropost model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Blog::Application.routes.draw do
   
+
+  resources :microposts
+
   resources :users
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130730125528) do
+ActiveRecord::Schema.define(version: 20130730142513) do
 
-# Could not dump table "MICROPOST" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "micro_posts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
